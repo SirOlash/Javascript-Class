69d233f566a63d4e6c7e5069b63b90a7da37044f
SirOlash <Olasupoemmanuel30@gmail.com>
2025-01-29 09:47:42 +0100

    first commit

---
+++ JavaScript Class/trial2.js
@@ -0,0 +1,30 @@
+let user = {
+	firstName : "Esther",
+	lastName : "jobless",
+	age : 13
+	greet : function(){
+			console.log("hello")
+		}
+};
+user.firstName = "Favour";
+user.sex = "male";
+
+//console.log(user)
+ 
+
+
+//let var1 = 2 ;
+//let var2 = 3;
+//let var3 = 4;
+
+//let varx;
+//varx = var2;
+//var2 = var3;
+//var3 = var1;
+
+
+
+
+//console.log(varx); 
+//console.log(var2); 
+//console.log(var3); 
---
+++ JavaScript Class/trials.js
@@ -0,0 +1,39 @@
+let xie = 4;
+let you = 5;
+
+let sum = xie + you;
+let squared = sum * sum;
+
+console.log(squared);
+
+let originalValue = 100;
+let remainder = 100 % 10;
+let result = remainder + originalValue;
+
+console.log(result);
+
+let number = 5;
+let n = 3;
+
+let result2 = 5**n;
+
+console.log(result2);
+
+
+let a = 25
+let x = a + 10;
+let y = a- 10;
+let z = a % 10;
+
+let b = a / (x*y*z)
+
+console.log(b.toFixed(7));
+
+
+
+
+
+
+
+
+
